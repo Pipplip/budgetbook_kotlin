ae0f38acd33cecc29d8a68da4183574000f16857
Philipp Becker <philipp_becker@gmx.de>
2025-12-26 11:52:32 +0100

    switch to JSON persistence

--- src/main/kotlin/budgetbook/Main.kt
+++ src/main/kotlin/budgetbook/Main.kt
@@ -1,17 +1,17 @@
 fun main() {
-//    val repository = JsonAccountRepository()
-//    if (repository is PersistableRepository) {
-//        repository.loadFromFile()
-//    }
+    val repository = JsonAccountRepository()
+    if (repository is PersistableRepository) {
+        repository.loadFromFile()
+    }
 
-    val repository = InMemoryAccountRepository()
+//    val repository = InMemoryAccountRepository()
     val service = AccountService(repository)
     val app = ConsoleApp(service)
 
     app.run()
 
-//    if (repository is PersistableRepository) {
-//        // Optional: Persist nach Ende
-//        repository.persistAllToFile()
-//    }
+    if (repository is PersistableRepository) {
+        // Optional: Persist nach Ende
+        repository.persistAllToFile()
+    }
 }
